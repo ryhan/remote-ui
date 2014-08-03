e5dd897fb5cea35fabd5da3ca250ee36296f16c7
Ryhan Hassan <ryhanh@me.com>
2014-08-03 01:16:19 -0700

    multiple views!

--- app/app.js
+++ app/app.js
@@ -29,51 +29,18 @@ $(function() {
     var icon_index = Number($(this).data("icon-id"));
     var new_icon_offset = -93*icon_index;
 
-    console.log(new_icon_offset);
 
     $(this).addClass('shrink');
     $(this).removeClass('grow');
 
     elementSpringTo("#caret", icon_index*95 + 45, 150, [50, 10, 2]);
     $('.popovers').addClass('bounce');
-  
-    
-    /*
-    var direction = (new_icon_offset < icon_offset) ? -1 : 1;
-
-    var bounce_icon_offset = direction*20 + new_icon_offset;
-    icon_offset = new_icon_offset;
-
-    var moveToXAt = function(x, time){
-      setTimeout(function(){
-        $app_icons.css({
-          "-webkit-transform": "translateX(" + x + "px)"
-        });
-      }, time);
-    };
-
-    moveToXAt(direction*40 + icon_offset, 0);
-    moveToXAt(direction*-20 + icon_offset, 200);
-     moveToXAt(icon_offset, 400);
-
-  */
-    /*
-    console.log(bounce_icon_offset);
-    console.log(new_icon_offset);
-    icon_offset = new_icon_offset;
-
-
-
-    $('.app_icon').css({
-      "-webkit-transform": "translateX(" + bounce_icon_offset + "px)"
-    });
 
     setTimeout(function(){
-      $('.app_icon').css({
-        "-webkit-transform": "translateX(" + new_icon_offset + "px)"
-      });
-    }, 200);
-    */
+      $('.controlset').removeClass('active');
+      $('.controlset[data-icon-id="'+icon_index + '"]').addClass('active');
+    }, 200);  
+    
 
   });
 
